35c2695855ccd21720d413785763f574b6110967
mpendulo <celempendulo@gmail.com>
2025-03-05 18:03:56 +0200

    Adding publish action

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,11 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.4.3"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("maven-publish")
 }
 
 group = "io.clopen"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 
 java {
 	toolchain {
