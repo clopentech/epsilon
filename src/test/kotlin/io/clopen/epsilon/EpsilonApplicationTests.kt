046298cb93cdc4875a5964864ec93933f4c2b7e7
mpendulo <celempendulo@gmail.com>
2025-03-06 08:50:29 +0200

    Adding a dummy test

---
+++ src/test/kotlin/io/clopen/epsilon/EpsilonApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.clopen.epsilon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EpsilonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
